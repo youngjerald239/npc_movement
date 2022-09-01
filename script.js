62740250863365c52c0e5cdcd2ea16165165035b
youngjerald239 <youngjerald239@gmail.com>
2022-08-31 19:17:43 -0500

    added square to canvas

---
+++ script.js
@@ -0,0 +1,17 @@
+/**  @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+CANVAS_WIDTH = canvas.width = 500
+CANVAS_HEIGHT = canvas.height = 1000
+
+enemy1 = {
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200,
+}
+
+function animate() {
+    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height)
+}
+animate()
