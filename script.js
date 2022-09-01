7f8ce497ffb4bedb64ab58802609fce5989a6155
youngjerald239 <youngjerald239@gmail.com>
2022-08-31 23:16:13 -0500

    added this.curve

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 CANVAS_WIDTH = canvas.width = 500
-CANVAS_HEIGHT = canvas.height = 1000
-const numberOfEnemies = 20
+CANVAS_HEIGHT = canvas.height = 10
+const numberOfEnemies = 200
 const enemiesArray = []
 // enemy1 = {
 //     x: 0,
@@ -28,13 +28,13 @@ class Enemy {
         this.y = Math.random() * (canvas.height - this.height)
         this.frame = 0
         this.flapSpeed = Math.floor(Math.random() * 3 + 1)
-        this.angle = 0
-        this.angleSpeed = Math.random() * 2
-        this.curve = Math.random() * 200
+        this.angle = Math.random() * 500
+        this.angleSpeed = Math.random() * 0.5 + 0.5
+        this.curve = Math.random() * 200 + 50
     }
     update(){
-        this.x = this.curve * Math.sin(this.angle * Math.PI/180) + (canvas.width/2 - this.width/2)
-        this.y = this.curve * Math.sin(this.angle * Math.PI/180) + (canvas.height/2 - this.height/2)
+        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2)
+        this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/500) + (canvas.height/2 - this.height/2)
         this.angle += this.angleSpeed
         if (this.x + this.width < 0) this.x = canvas.width
         // animate sprites
