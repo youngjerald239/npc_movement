e360193857f76b5b5a1d8babecb06e17ba2b5c3c
youngjerald239 <youngjerald239@gmail.com>
2022-08-31 21:06:52 -0500

    added update function

--- script.js
+++ script.js
@@ -18,14 +18,19 @@ class Enemy {
         this.width = 100
         this.height = 100
     }
+    update(){
+        this.x++
+        this.y++
+    }
 }
 const enemy1 = new Enemy()
+const enemy2 = new Enemy()
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    enemy1.x++
-    enemy1.y++
+    enemy1.update()
     ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height)
+    ctx.fillRect(enemy2.x, enemy2.y, enemy2.width, enemy2.height)
     requestAnimationFrame(animate)
 }
 animate()
