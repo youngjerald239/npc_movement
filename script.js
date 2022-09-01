f9d2a9ee384abe8f441f159d6f29169dc63c1009
youngjerald239 <youngjerald239@gmail.com>
2022-08-31 19:21:22 -0500

    show animation frame

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ CANVAS_HEIGHT = canvas.height = 1000
 
 enemy1 = {
     x: 0,
-    y: 0,
+    y: 50,
     width: 200,
     height: 200,
 }
 
 function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    enemy1.x++
     ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height)
+    requestAnimationFrame(animate)
 }
 animate()
