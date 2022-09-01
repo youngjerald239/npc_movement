c092cecfd12eaa4d40bf871eda1d29598627dcf6
youngjerald239 <youngjerald239@gmail.com>
2022-08-31 19:25:35 -0500

    created enemy class with animate function

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ const ctx = canvas.getContext('2d')
 CANVAS_WIDTH = canvas.width = 500
 CANVAS_HEIGHT = canvas.height = 1000
 
-enemy1 = {
-    x: 0,
-    y: 50,
-    width: 200,
-    height: 200,
+// enemy1 = {
+//     x: 0,
+//     y: 50,
+//     width: 200,
+//     height: 200,
+// }
+
+class Enemy {
+    constructor() {
+        this.x = 10
+        this.y = 50
+        this.width = 100
+        this.height = 100
+    }
 }
+const enemy1 = new Enemy()
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     enemy1.x++
+    enemy1.y++
     ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height)
     requestAnimationFrame(animate)
 }
